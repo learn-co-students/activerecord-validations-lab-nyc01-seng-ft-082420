23b95beedd8a26dab59c9ab62a2db54c85fad5ee
sdonohue0918 <sdonohue0918@gmail.com>
2020-09-17 18:32:52 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,24 @@
 class Post < ActiveRecord::Base
+    # include ActiveModel::Validations
+    # validates_with CategoryValidator
+    validates :title, presence: true
+    validates :content, {:length => {:minimum => 20}}
+    validates :summary, {:length => {:maximum => 200}}
+    # validates :category, inclusion: { in: %w(post tag image update)}
+    #validate :is_clickbait?
+    
 end
+
+
+# CLICKBAIT_PATTERNS = [
+#     /Won't Believe/i,
+#     /Secret/i,
+#     /Top [0-9]*/i,
+#     /Guess/i
+#   ]
+
+#   def is_clickbait?
+#     if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+#       errors.add(:title, "must be clickbait")
+#     end
+#   end
