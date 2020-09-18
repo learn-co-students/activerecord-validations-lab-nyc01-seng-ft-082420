6ce8fbed0ac36bc569539c408cfdc05fb283dc61
jdhofmann <justindhofmann@gmail.com>
2020-09-18 13:28:42 -0400

    done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,11 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {
+  minimum: 250
+  }
+  validates :summary, length: {
+  maximum: 250
+  }
+  validates :category, inclusion: { in: [ "Fiction", "Non-Fiction"] }
+  # validates :id, :created_at, :updated_at, absence: true
 end
